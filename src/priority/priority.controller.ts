29ddcacf02733b6ddb47f32962c38585f6f2e6f2
Evgeniy <edubskiy@openwaygroup.com>
2021-08-13 20:25:38 +0300

    Added reset/recound/move priority API

--- src/priority/priority.controller.ts
+++ src/priority/priority.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { PriorityService } from './priority.service';
+import { Controller, Get } from '@nestjs/common';
+import { GetPriorityDTO } from './dto/get-priority.dto';
 
 @Controller('priority')
-export class PriorityController {}
+export class PriorityController {
+  constructor(
+    private priorityService: PriorityService
+  ) {}
+
+  @Get('/program')
+  priority(getPriorityDTO: GetPriorityDTO) {
+    return this.priorityService.resetPriority();
+  }
+
+}
---
+++ src/priority/priority.library.ts
@@ -0,0 +1,11 @@
+// rebuilds all tree indexes from scratch
+// Cases: TODO
+export const rebuildPriority = () => {}
+
+// rebuilds all tree indexes based on existing priority provided
+// Cases: TODO
+export const recalculatePriority = () => {}
+
+// moves priority to new index and rebuilds all leaf indexes
+// Cases: TODO
+export const movePriority = () => {}
